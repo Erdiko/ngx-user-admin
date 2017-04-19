ec47b1e6efc384687386a171f31202333ec89dfa
Andy Armstrong <andyarmstro@gmail.com>
2017-04-19 15:09:40 -0700

    init commit

---
+++ src/user-admin.component.ts
@@ -0,0 +1,14 @@
+import {Component} from '@angular/core';
+
+@Component({
+    selector: 'user-admin',
+    styles: [`
+
+    `],
+    template: `
+    <h1>User Admin Test</h1>
+`
+})
+export class UserAdminComponent {
+
+}
