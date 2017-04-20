75257c7b2099562b5429014ca334322e50d02486
Andy Armstrong <andyarmstro@gmail.com>
2017-04-19 17:16:36 -0700

    updated entry points

--- index.d.ts
+++ index.d.ts
@@ -1,3 +1,3 @@
 export { UserAdminModule }      from './lib/user-admin.module';
 
-export { UserAdminComponent }   from './lib/user-admin.component';
+export { UserListComponent }    from './lib/user-list.component';
