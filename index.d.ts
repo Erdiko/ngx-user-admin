1d95185a5d564c6fd352fa2fd5ac860eb114d8c4
Andy Armstrong <andyarmstro@gmail.com>
2017-04-19 18:23:51 -0700

    deleted stuff

--- index.d.ts
+++
@@ -1,5 +0,0 @@
-export { UserAdminModule }      from './lib/user-admin.module';
-
-export { UsersService }         from './lib/users.service';
-
-export { UserListComponent }    from './lib/user-list.component';
