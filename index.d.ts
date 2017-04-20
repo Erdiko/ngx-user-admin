30c5049cb31d60aa8ebd985c5660135a37426b11
Andy Armstrong <andyarmstro@gmail.com>
2017-04-19 18:14:11 -0700

    more tweaks

--- index.d.ts
+++ index.d.ts
@@ -1,3 +1,5 @@
 export { UserAdminModule }      from './lib/user-admin.module';
 
+export { UsersService }         from './lib/users.service';
+
 export { UserListComponent }    from './lib/user-list.component';
