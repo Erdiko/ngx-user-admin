99720d5cef2e896bfb9b23e07408f0307152822a
Andy Armstrong <andyarmstro@gmail.com>
2017-05-31 14:13:01 -0700

    first pass at rollup

--- rollup.config.js
+++ rollup.config.js
@@ -1,3 +1,15 @@
+import angular from 'rollup-plugin-angular';
+import typescript from 'rollup-plugin-typescript2';
+import nodeResolve from 'rollup-plugin-node-resolve';
+import commonjs from 'rollup-plugin-commonjs';
+//import { minify as minifyHtml } from 'html-minifier';
+ 
+const htmlminOpts = {
+    caseSensitive: true,
+    collapseWhitespace: true,
+    removeComments: true,
+};
+
 export default {
   entry: 'dist/index.js',
   dest: 'dist/bundles/ng-user-admin.umd.js',
@@ -5,6 +17,16 @@ export default {
   format: 'umd',
   moduleName: 'ng.ng-user-admin',
   external: ['@angular/http'],
+
+  plugins: [
+    angular(),
+    commonjs({
+      include: 'node_modules/**'
+    }),
+    typescript(),
+    nodeResolve({ jsnext: true, main: true })
+  ],
+
   globals: {
     '@angular/core': 'ng.core',
     '@angular/http' : 'vendor._angular_http',
