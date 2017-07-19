bc1232773011c65a74e16a1a53719df10ccc1a9c
Andy Armstrong <andyarmstro@gmail.com>
2017-07-18 22:45:01 -0700

    erdiko/ngx-user-admin#62: more specs
    added specs for home, header and app components

--- src/app.component.spec.ts
+++ src/app.component.spec.ts
@@ -1,21 +1,71 @@
-import { TestBed, ComponentFixture } from '@angular/core/testing';
-import { AppComponent } from './app.component';
+/* tslint:disable:no-unused-variable */
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { DebugElement } from '@angular/core';
 
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
-describe('component: AppComponent', () => {
-  let fixture: ComponentFixture<AppComponent>;
+import { HttpModule }           from '@angular/http';
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({
-      declarations: [ AppComponent ]
+import { AppComponent }         from './app.component';
+import { HeaderComponent }      from './header/header.component';
+import { AuthService }          from './auth.service';
+import { UsersService }         from './users.service';
+import { MessageService }       from './message.service';
+import { UserResolve }          from './user-resolve.service';
+
+describe('AppComponent', () => {
+
+    let component: AppComponent;
+    let fixture: ComponentFixture<AppComponent>;
+
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            declarations: [
+                AppComponent,
+                HeaderComponent
+            ],
+            imports: [
+                HttpModule
+            ],
+            providers: [
+                AuthService,
+                UsersService,
+                UserResolve,
+                MessageService
+            ],
+            schemas:  [CUSTOM_ELEMENTS_SCHEMA]
+        });
+        TestBed.compileComponents();
+    }));
+
+    beforeEach(() => {
+        fixture = TestBed.createComponent(AppComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
     });
-    fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-  });
 
-  it('should display Hello World message', () => {
-    const debugEl = fixture.debugElement;
-    const h1 = debugEl.nativeElement.querySelector('h1');
-    expect(h1.textContent).toEqual('Hello World!');
-  });
+    it('should create the app', async(() => {
+        fixture.detectChanges();
+        let app = fixture.debugElement.componentInstance;
+        const compiled = fixture.debugElement.nativeElement;
+        expect(app).toBeTruthy();
+    }));
+
+    it('should include and render the header component', async(() => {
+        fixture.detectChanges();
+        let app = fixture.debugElement.componentInstance;
+        const compiled = fixture.debugElement.nativeElement;
+
+        expect(compiled.querySelectorAll('app-header')).toBeTruthy();
+    }));
+
+    it('should include and render a router outlet', async(() => {
+        fixture.detectChanges();
+        let app = fixture.debugElement.componentInstance;
+        const compiled = fixture.debugElement.nativeElement;
+
+        expect(compiled.querySelectorAll('router-outlet')).toBeTruthy();
+    }));
+
 });
